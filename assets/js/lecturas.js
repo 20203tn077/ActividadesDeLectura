801d244b7b532175c773c4a2a7669021a8d6098b
nekashii <20203tn077@utez.edu.mx>
2022-02-10 00:21:49 -0600

    Versión funcional de la página

--- assets/js/lecturas.js
+++ assets/js/lecturas.js
@@ -1,18 +1,45 @@
 const lecturas = [
     [
         {
-            titulo: "Título de la lectura 11",
-            lectura: "Lorem ipsum dolor sit amet con<br>sectetur adipisicing elit. Laudantium perferendis, dolore provident dolorum voluptatem, tenetur sapiente et nam soluta iure eligendi  dolorem mollitia quis ab illum similique debitis dignissimos qui? Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ullam eligendi voluptates adipisci error, quasi aliquam eveniet reprehenderit totam, natus enim odit eaque porro corporis dicta minima culpa recusandae nobis. Ducimus! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corrupti nostrum deserunt cum similique consequuntur incidunt optio repudiandae beatae! Enim ducimus quia deleniti, laboriosam quisquam in voluptatum necessitatibus earum dolor est?",
-            numPalabras: 12,
+            titulo: "El gato negro",
+            lectura: 'Dos gatitos, nada más, había tenido la gata de Doña Casimira Vallejo, y ya habían pedido a la citada señora nada menos que catorce. Y es que los gatitos eran completamente negros, y sabido es que hay muchas personas que creen que aquellos traen la felicidad a las casas.<br><br>De buena gana Doña Casimira no se hubiera desprendido de aquellos dos hijos de su Sultana; pero su esposo le había declarado que no quería más gatos en su vivienda, y la buena señora tuvo que resignarse a regalarlos el día mismo que cumplieran dos meses.<br><br>Mucho tiempo estuvo pensando dónde quedarían mejor colocados; el vecino del piso bajo perdía muchos gatos y no faltaba quien sospechase que se los comía; el tendero de enfrente los dejaba salir a la calle y se los robaban; la vieja del cuarto entresuelo era muy económica y no les daba de comer; el cura tenía un perro que asustaba a los animalitos; y así, de uno en otro, resultó que los catorce pedidos se redujeron para Doña Casimira solamente a dos, casualmente el número de gatos que tenía. Aun así, no acabaron sus cavilaciones.<br><br>Moro, el más hermoso y más grave de los dos gatitos, convendría mejor a Doña Carlota, la vecina del tercero de la izquierda, que tenía una hija muy juiciosa a pesar de sus cortos años; pero Fígaro (así nombrado por el marido de Doña Casimira por haberle hallado un día jugando con su guitarra), no estaría del todo bien en casa de don Serafín, cuyos niños eran muy revoltosos y trataban con dureza a los animales.<br><br>Pero al cabo, como el tiempo urgía, Morito fue entregado a Doña Carlota y Fígaro a Don Serafín.<br><br>Ambos fueron adornados con collares rojos y cascabeles, y Blanca, la niña de la viuda, y Alejandro y Pepita, hijos del caballero, que también era vecino de Doña Casimira, habitando en el otro tercero, no dudaron ya que en sus moradas todo sería bienestar y ventura con haber llevado a ellas a los dos gatitos.<br><br>Al pronto la casualidad vino a confirmar aquella idea: Doña Carlota ganó un premio a la lotería y D. Serafín, que estaba cesante, fue colocado con doce mil reales en un Ministerio.<br><br>-¡El gato negro! -exclamaban los chicos.<br><br>-¡El gato negro!<br><br>Lo que no impedía que Alejandro y Pepita maltratasen al pobre Fígaro, que, cuando podía, se vengaba de ellos clavando en sus manos los dientes o las uñas; pero como era tan pequeño no les hacía gran daño.<br><br>En cambio Morito pasaba los días en la falda de su joven ama y las noches en un colchoncito muy blando que hizo Blanca para el gato en cuanto se lo dieron. Demostraba él su contento con ese ronquido acompasado que en los gatos es indicio de felicidad completa, y es seguro que si hubiese sabido hablar no hubiera dejado de decir a Doña Casimira que no podía haberle proporcionado una casa mejor.',
+            numPalabras: 491,
             preguntas: [
                 {
-                    pregunta: "¿Hola?",
+                    pregunta: "¿Cuántos gatitos tenía encargados Doña Casimira?",
                     opciones: [
-                        "opcion1",
-                        "opcion2",
-                        "opcion3",
+                        "Dos.",
+                        "Siete.",
+                        "Catorce.",
+                        "Veinte.",
                     ],
-                    respuesta: "opcion2"
+                    respuesta: "Catorce."
+                },
+                {
+                    pregunta: "¿Cómo se llamaba su gata, la madre de los gatitos?",
+                    opciones: [
+                        "Sultana.",
+                        "Blanca.",
+                    ],
+                    respuesta: "Sultana."
+                },
+                {
+                    pregunta: "¿Por qué la gente quería tener un gato negro?",
+                    opciones: [
+                        "Porque son más cariñosos.",
+                        "Porque traen buena suerte.",
+                        "Porque son mejores cazadores.",
+                    ],
+                    respuesta: "Porque traen buena suerte."
+                },
+                {
+                    pregunta: "¿A quién le robaban los gatos?",
+                    opciones: [
+                        "Al tendero.",
+                        "Al vecino del piso bajo.",
+                        "Al cura.",
+                    ],
+                    respuesta: "Al tendero."
                 },
             ]
         },
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,38 @@
-// VARIABLES
+// ELEMENTOS
 // Secciones
-let secActividad = document.getElementById('secActividad');
-let secVelocidad = document.getElementById('lectura');
-let contenedorLectura = document.getElementById('contenedorLectura');
+const secActividad = document.getElementById('secActividad');
+const secVelocidad = document.getElementById('lectura');
+
+// Contenedores
+const contenedorLectura = document.getElementById('contenedorLectura');
+const contenedorPreguntas = document.getElementById('contenedorPreguntas');
 
 // Botones
-let btnContinuarInstrucciones = document.getElementById('btnContinuarInstrucciones');
-let btnEmpezarVelocidad = document.getElementById('btnEmpezarVelocidad');
-let btnTerminarVelocidad = document.getElementById('btnTerminarVelocidad');
-let btnTerminarComprension = document.getElementById('btnTerminarComprension');
-let btnReiniciar = document.getElementById('btnReiniciar');
-let btnNuevaLectura = document.getElementById('btnNuevaLectura');
+const btnContinuarInstrucciones = document.getElementById('btnContinuarInstrucciones');
+const btnEmpezarVelocidad = document.getElementById('btnEmpezarVelocidad');
+const btnTerminarVelocidad = document.getElementById('btnTerminarVelocidad');
+const btnTerminarComprension = document.getElementById('btnTerminarComprension');
+const btnReiniciar = document.getElementById('btnReiniciar');
+const btnNuevaLectura = document.getElementById('btnNuevaLectura');
 
 // Cadenas de texto
-let txtTitulo = document.getElementById('txtTitulo');
-let txtLectura = document.getElementById('txtLectura');
-let txtContador = document.getElementById('txtContador');
-let txtMinutosVelocidad = document.getElementById('txtMinutosVelocidad');
-let txtPalabrasVelocidad = document.getElementById('txtPalabrasVelocidad');
+const txtTitulo = document.getElementById('txtTitulo');
+const txtLectura = document.getElementById('txtLectura');
+const txtContador = document.getElementById('txtContador');
+const txtMinutosVelocidad = document.getElementById('txtMinutosVelocidad');
+const txtPalabrasVelocidad = document.getElementById('txtPalabrasVelocidad');
+const txtPuntuacionComprension = document.getElementById('txtPuntuacionComprension');
 
 // Resultados
-let resultadosVelocidad = document.getElementById('resultadosVelocidad');
-let resultadosComprension = document.getElementById('resultadosComprension');
+const resultadosVelocidad = document.getElementById('resultadosVelocidad');
+const resultadosComprension = document.getElementById('resultadosComprension');
 
-let contenedorPreguntas = document.getElementById('contenedorPreguntas');
-let = document;
-let = document;
-let = document;
-let = document;
-let = document;
-
-
+// VARIABLES
 let lecturasActual;
 let lecturaActual;
-
-let contadorSegundos = 0;
-let contadorMinutos = 0;
+let procesoContador;
+let contadorSegundos;
+let contadorMinutos;
 
 // FUNCIONES
 function obtenerLectura() {
@@ -52,23 +49,37 @@ function obtenerNuevaLectura() {
 }
 
 function mostrarLectura() {
-    if (typeof procesoContador !== 'undefined') clearInterval(procesoContador);
+    if ( procesoContador ) {
+        clearInterval(procesoContador);
+    }
     const {titulo, lectura} = lecturaActual;
+
     btnTerminarVelocidad.style.display = 'none';
     resultadosVelocidad.style.display = 'none';
     resultadosComprension.style.display = 'none';
     txtContador.style.display = 'none';
     btnEmpezarVelocidad.style.display = 'initial';
+    btnTerminarComprension.style.display = 'initial';
+
     txtContador.innerHTML = '00:00';
     txtTitulo.innerHTML = titulo;
     txtLectura.innerHTML = lectura;
+
+    let contenido = '';
+    lecturaActual.preguntas.map(({pregunta, opciones}, numPregunta) => {
+        contenido += `<div class="mb-3"><label class="form-label">${pregunta}</label>`
+        opciones.map((opcion, numOpcion) => {
+            contenido += `<div class="form-check"><input class="form-check-input" type="radio" value="${opcion}" name="pregunta${numPregunta}" id="pregunta${numPregunta}opcion${numOpcion}"><label class="form-check-label" for="pregunta${numPregunta}opcion${numOpcion}">${opcion}</label></div>`;
+        });
+        contenido += '</div>';
+    });
+    contenedorPreguntas.innerHTML = contenido;
     secActividad.scrollIntoView({block: "start", behavior: "smooth"});
 }
 
 function obtenerLecturaPorNivel(nivel) {
     lecturasActual = lecturas[nivel - 1];
-    lecturaActual = obtenerLectura();
-    mostrarLectura();
+    obtenerNuevaLectura();
 }
 
 // LISTENERS
@@ -102,7 +113,7 @@ btnEmpezarVelocidad.onclick = () => {
 btnTerminarVelocidad.onclick = () => {
     clearInterval(procesoContador);
     txtMinutosVelocidad.innerHTML = txtContador.innerHTML;
-    txtPalabrasVelocidad.innerHTML = (lecturaActual.numPalabras/(contadorMinutos + contadorSegundos/60));
+    txtPalabrasVelocidad.innerHTML = Math.round((lecturaActual.numPalabras/(contadorMinutos + contadorSegundos/60)) * 10) / 10;
     
     btnTerminarVelocidad.style.display = 'none';
     resultadosVelocidad.style.display = 'initial';
@@ -110,7 +121,29 @@ btnTerminarVelocidad.onclick = () => {
     resultadosVelocidad.scrollIntoView({block: "center", behavior: "smooth"});
 }
 
+btnTerminarComprension.onclick = () => {
+    let aciertos = 0;
+    const {preguntas} = lecturaActual;
+    preguntas.map(({respuesta}, numPregunta) => {
+        btnTerminarComprension.style.display = 'none';
+        Array.from(document.getElementsByName(`pregunta${numPregunta}`)).map((opcion) => {
+            opcion.disabled = true;
+        })
+        let opcionElegida = document.querySelector(`[name="pregunta${numPregunta}"]:checked`);
+        if ( opcionElegida ) {
+            if (opcionElegida.value == respuesta) {
+                opcionElegida.classList.add('is-valid');
+                aciertos++;
+            } else opcionElegida.classList.add('is-invalid');
+        }
+    });
+    txtPuntuacionComprension.innerHTML = `${Math.round((aciertos/preguntas.length) * 100)}%`;
+    resultadosComprension.style.display = 'initial';
+    resultadosComprension.scrollIntoView({block: "center", behavior: "smooth"});
+}
+
 btnReiniciar.onclick = mostrarLectura;
 btnNuevaLectura.onclick = obtenerNuevaLectura;
 
+// INICIO
 obtenerLecturaPorNivel(1);
